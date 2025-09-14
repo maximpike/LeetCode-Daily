c53468f86ba0e917cbd6a15816440434c958d835
maxpike <maximilianpike@gmail.com>
2025-09-14 16:58:59 +0200

    Solved Leetcode Problem 1929: Concatenation of Array

---
+++ src/main/java/org/cl/easy/ConcatenationOfArray.java
@@ -0,0 +1,30 @@
+package org.cl.easy;
+
+public class ConcatenationOfArray {
+
+    // ---------------------- SOLUTION 1 -----------------------------//
+    public int[] getConcatenation_solution1(int[] nums) {
+
+        int[] ans = new int[nums.length*2];
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+        return ans;
+    }
+    // Time Complexity = O(2n) ~ = O(n) - Each iteration does 2 assignments in constant time (total operations = 2n).
+    // Space Complexity = O(2n) ~ = O(n) - we are doubling the size of new output array (n & i are equal to O(1))
+
+    // ---------------------- SOLUTION 2 -----------------------------//
+    public int[] getConcatenation_solution2(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n*2];
+        System.arraycopy(nums,0, ans,0,n);
+        System.arraycopy(nums, 0, ans, n, n);
+        return ans;
+    }
+    // Time Complexity = O(2n) - System.arraycopy copies n elements in one call.
+    //                 = O(n)    We call it twice, so it copies 2n elements total.
+    // Space Complexity = O(2n) ~ O(n) - NEW ARRAY therefore requires O(n) extra space
+}
---
+++ src/test/java/org/cl/easy/ConcatenationOfArrayTest.java
@@ -0,0 +1,55 @@
+package org.cl.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConcatenationOfArrayTest {
+
+    @Test
+    void getConcatenation_1x3Array() {
+        int[] nums = new int[]{1, 2, 1};
+        int[] expected = new int[]{1, 2, 1, 1, 2, 1};
+        ConcatenationOfArray c = new ConcatenationOfArray();
+        assertEquals(expected.length, c.getConcatenation_solution1(nums).length);
+        assertEquals(expected.length, c.getConcatenation_solution2(nums).length);
+        assertArrayEquals(expected, c.getConcatenation_solution1(nums));
+        assertArrayEquals(expected, c.getConcatenation_solution2(nums));
+    }
+
+    @Test
+    void getConcatenation_1x4Array() {
+        int[] nums = new int[]{1, 3, 2, 1};
+        int[] expected = new int[]{1, 3, 2, 1, 1, 3, 2, 1};
+        ConcatenationOfArray c = new ConcatenationOfArray();
+        assertEquals(expected.length, c.getConcatenation_solution1(nums).length);
+        assertEquals(expected.length, c.getConcatenation_solution2(nums).length);
+        assertArrayEquals(expected, c.getConcatenation_solution1(nums));
+        assertArrayEquals(expected, c.getConcatenation_solution2(nums));
+    }
+
+    @Test
+    void getConcatenation_1x5Array() {
+        int[] nums = new int[]{103, 301, 222, 103, 600};
+        int[] expected = new int[]{103, 301, 222, 103, 600, 103, 301, 222, 103, 600};
+        ConcatenationOfArray c = new ConcatenationOfArray();
+        assertEquals(expected.length, c.getConcatenation_solution1(nums).length);
+        assertEquals(expected.length, c.getConcatenation_solution2(nums).length);
+        assertArrayEquals(expected, c.getConcatenation_solution1(nums));
+        assertArrayEquals(expected, c.getConcatenation_solution2(nums));
+    }
+
+    @Test
+    void getConcatenation_1x6Array() {
+        int[] nums = new int[]{1, 21, 400, 5, 1, 999};
+        int[] expected = new int[]{1, 21, 400, 5, 1, 999, 1, 21, 400, 5, 1, 999};
+        ConcatenationOfArray c = new ConcatenationOfArray();
+        assertEquals(expected.length, c.getConcatenation_solution1(nums).length);
+        assertEquals(expected.length, c.getConcatenation_solution2(nums).length);
+        assertArrayEquals(expected, c.getConcatenation_solution1(nums));
+        assertArrayEquals(expected, c.getConcatenation_solution2(nums));
+    }
+
+    // TODO: create stress test
+
+}
