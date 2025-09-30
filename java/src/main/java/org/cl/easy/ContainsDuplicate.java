afb96ab6927653963a4fa07bfa70a55ec8a8bf4f
maxpike <maximilianpike@gmail.com>
2025-09-30 14:48:08 +0200

    feat: solve leetcode problem 217 in java
    problem name: contains duplicate

---
+++ java/src/main/java/org/cl/easy/ContainsDuplicate.java
@@ -0,0 +1,34 @@
+package org.cl.easy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+
+    // ---------------------- SOLUTION 1 -----------------------------//
+    public boolean containsDuplicate_solution1(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num) && map.get(num) >= 1) {
+                return true;
+            }
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        return false;
+    }
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+
+    // ---------------------- SOLUTION 1 - Use HashSet -----------------------------//
+    public boolean containsDuplicate_solution2(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
---
+++ java/src/test/java/org/cl/easy/ContainsDuplicateTest.java
@@ -0,0 +1,43 @@
+package org.cl.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainsDuplicateTest {
+
+    @Test
+    void containsDuplicate_example1() {
+        ContainsDuplicate cd = new ContainsDuplicate();
+        assertTrue(cd.containsDuplicate_solution1(new int[]{1, 2, 3, 1}));
+        assertTrue(cd.containsDuplicate_solution2(new int[]{1, 2, 3, 1}));
+    }
+
+    @Test
+    void containsDuplicate_example2() {
+        ContainsDuplicate cd = new ContainsDuplicate();
+        assertFalse(cd.containsDuplicate_solution1(new int[]{1, 2, 3, 4}));
+        assertFalse(cd.containsDuplicate_solution2(new int[]{1, 2, 3, 4}));
+    }
+
+    @Test
+    void containsDuplicate_example3() {
+        ContainsDuplicate cd = new ContainsDuplicate();
+        assertTrue(cd.containsDuplicate_solution1(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+        assertTrue(cd.containsDuplicate_solution2(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+    }
+
+    @Test
+    void containsDuplicate_example4() {
+        ContainsDuplicate cd = new ContainsDuplicate();
+        assertFalse(cd.containsDuplicate_solution1(new int[]{-2, -1, 0, 1, 2}));
+        assertFalse(cd.containsDuplicate_solution2(new int[]{-2, -1, 0, 1, 2}));
+    }
+
+    @Test
+    void containsDuplicate_example5() {
+        ContainsDuplicate cd = new ContainsDuplicate();
+        assertTrue(cd.containsDuplicate_solution1(new int[]{-1, 0, -1, 0}));
+        assertTrue(cd.containsDuplicate_solution2(new int[]{-1, 0, -1, 0}));
+    }
+}
