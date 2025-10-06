fa3676c11348ff21857659767ea04b9f3f454a6c
maxpike <maximilianpike@gmail.com>
2025-10-06 12:54:41 +0200

    feat: solve leetcode problem 283 in java
    problem name: move zeros

---
+++ java/src/main/java/org/cl/easy/MoveZeros.java
@@ -0,0 +1,20 @@
+package org.cl.easy;
+
+
+public class MoveZeros {
+
+    // ---------------------- SOLUTION 1  -----------------------------//
+    public void moveZeroes(int[] nums) {
+
+        int l = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] != 0) {
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+            }
+        }
+    }
+}
---
+++ java/src/test/java/org/cl/easy/MoveZerosTest.java
@@ -0,0 +1,58 @@
+package org.cl.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class MoveZerosTest {
+
+    @Test
+    void moveZeroes_example1() {
+        int[] nums = {0, 1, 0, 3, 12};
+
+        MoveZeros mz = new MoveZeros();
+        mz.moveZeroes(nums);
+
+        assertArrayEquals(new int[] {1,3,12,0,0}, nums);
+    }
+
+    @Test
+    void moveZeroes_example2() {
+        int[] nums = {0};
+
+        MoveZeros mz = new MoveZeros();
+        mz.moveZeroes(nums);
+
+        assertArrayEquals(new int[] {0}, nums);
+    }
+
+    @Test
+    void moveZeroes_example3() {
+        int[] nums = {1,2,3,4};
+
+        MoveZeros mz = new MoveZeros();
+        mz.moveZeroes(nums);
+
+        assertArrayEquals(new int[] {1,2,3,4}, nums);
+    }
+
+    @Test
+    void moveZeroes_example4() {
+        int[] nums = {0,0,0,0};
+
+        MoveZeros mz = new MoveZeros();
+        mz.moveZeroes(nums);
+
+        assertArrayEquals(new int[] {0,0,0,0}, nums);
+    }
+
+    @Test
+    void moveZeroes_example() {
+        int[] nums = {};
+
+        MoveZeros mz = new MoveZeros();
+        mz.moveZeroes(nums);
+
+        assertArrayEquals(new int[]{}, nums);
+    }
+}
