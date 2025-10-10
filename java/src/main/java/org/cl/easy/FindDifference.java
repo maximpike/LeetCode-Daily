199fea1cf78ccdef027d93f5aa9871431a6e0ec4
maxpike <maximilianpike@gmail.com>
2025-10-10 10:45:08 +0200

    refactor: add solution to 389 find the difference and test clean up

--- java/src/main/java/org/cl/easy/FindDifference.java
+++ java/src/main/java/org/cl/easy/FindDifference.java
@@ -1,12 +1,13 @@
 package org.cl.easy;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class FindDifference {
 
     // ---------------------- SOLUTION 1 -----------------------------//
-    public char findTheDifference(String s, String t) {
+    public char findTheDifference_solution1(String s, String t) {
 
         HashMap<Character, Integer> map = new HashMap<>();
 
@@ -24,10 +25,30 @@ public class FindDifference {
         }
 
         Set<Character> set = map.keySet();
-        return set.iterator().next();
+        return (Character) set.toArray()[0]; // Casts Object → Character, then auto-unboxed to char for return
     }
     // Time Complexity: O(s+t) = O(n)
     // Space Complexity: O(n+1) = O(n)
 
-    // ---------------------- SOLUTION 2 - Ascii values -----------------------------//
+    // ---------------------- SOLUTION 2 - Optimised -----------------------------//
+    public char findTheDifference_solution2(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.get(c) - 1);
+            if (map.get(c) == 0) {
+                map.remove(c);
+            }
+        }
+
+        return map.keySet().iterator().next();
+    }
+    // Time Complexity: O(n)
+    // Space Complexity: O(t) - t number of unique characters in String t
+
+    // ---------------------- SOLUTION 3 - Ascii values -----------------------------//
 }
--- java/src/test/java/org/cl/easy/FindDifferenceTest.java
+++ java/src/test/java/org/cl/easy/FindDifferenceTest.java
@@ -8,15 +8,22 @@ class FindDifferenceTest {
 
     @Test
     void findTheDifference_example1() {
-
         FindDifference fd = new FindDifference();
-        assertEquals('e', fd.findTheDifference("abcd", "abcde"));
+        assertEquals('e', fd.findTheDifference_solution1("abcd", "abcde"));
+        assertEquals('e', fd.findTheDifference_solution2("abcd", "abcde"));
     }
 
     @Test
     void findTheDifference_example2() {
-
         FindDifference fd = new FindDifference();
-        assertEquals('y', fd.findTheDifference("", "y"));
+        assertEquals('y', fd.findTheDifference_solution1("", "y"));
+        assertEquals('y', fd.findTheDifference_solution2("", "y"));
+    }
+
+    @Test
+    void findTheDifference_example3() {
+        FindDifference fd = new FindDifference();
+        assertEquals('a', fd.findTheDifference_solution1("a", "aa"));
+        assertEquals('a', fd.findTheDifference_solution2("a", "aa"));
     }
 }
