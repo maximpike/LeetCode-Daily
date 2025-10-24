a005890712a0c7b5ee37bbafa5cf708a78dc987c
maxpike <maximilianpike@gmail.com>
2025-10-24 22:54:29 +0200

    refactor: code clean up for reverse string

--- java/src/main/java/org/cl/easy/ReverseString.java
+++ java/src/main/java/org/cl/easy/ReverseString.java
@@ -3,10 +3,9 @@ package org.cl.easy;
 public class ReverseString {
 
     public void reverseString(char[] s) {
-
         int l = 0;
         int r = s.length - 1;
-        while (l <= r) {
+        while (l < r) {
             char temp = s[l];
             s[l] = s[r];
             s[r] = temp;
