094e6965d40fd550c0b5b7aa7fbbe5c9679bf928
maxpike <maximilianpike@gmail.com>
2025-10-24 22:53:35 +0200

    refactor: added solution and additional test cases for move zeros

--- java/src/main/java/org/cl/easy/MoveZeros.java
+++ java/src/main/java/org/cl/easy/MoveZeros.java
@@ -1,13 +1,10 @@
 package org.cl.easy;
 
-
 public class MoveZeros {
 
     // ---------------------- SOLUTION 1  -----------------------------//
-    public void moveZeroes(int[] nums) {
-
+    public void moveZeroes_solution1(int[] nums) {
         int l = 0;
-
         for (int r = 0; r < nums.length; r++) {
             if (nums[r] != 0) {
                 int temp = nums[l];
@@ -17,4 +14,23 @@ public class MoveZeros {
             }
         }
     }
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+
+    // ---------------------- SOLUTION 2  -----------------------------//
+    public void moveZeroes_solution2(int[] nums) {
+        int l = 0;
+        int r = 0;
+        while (r < nums.length) {
+            if (nums[r] != 0) {
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+            }
+            r++;
+        }
+    }
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
 }
--- java/src/test/java/org/cl/easy/MoveZerosTest.java
+++ java/src/test/java/org/cl/easy/MoveZerosTest.java
@@ -6,43 +6,85 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class MoveZerosTest {
 
+    // Solution 1 - Tests
     @Test
-    void moveZeroes_example1() {
+    void moveZeroes_solution1_example1() {
         int[] nums = {0, 1, 0, 3, 12};
         MoveZeros mz = new MoveZeros();
-        mz.moveZeroes(nums);
+        mz.moveZeroes_solution1(nums);
         assertArrayEquals(new int[] {1,3,12,0,0}, nums);
     }
 
     @Test
-    void moveZeroes_example2() {
+    void moveZeroes_solution1_example2() {
         int[] nums = {0};
         MoveZeros mz = new MoveZeros();
-        mz.moveZeroes(nums);
+        mz.moveZeroes_solution1(nums);
         assertArrayEquals(new int[] {0}, nums);
     }
 
     @Test
-    void moveZeroes_example3() {
+    void moveZeroes_solution1_example3() {
         int[] nums = {1,2,3,4};
         MoveZeros mz = new MoveZeros();
-        mz.moveZeroes(nums);
+        mz.moveZeroes_solution1(nums);
         assertArrayEquals(new int[] {1,2,3,4}, nums);
     }
 
     @Test
-    void moveZeroes_example4() {
+    void moveZeroes_solution1_example4() {
         int[] nums = {0,0,0,0};
         MoveZeros mz = new MoveZeros();
-        mz.moveZeroes(nums);
+        mz.moveZeroes_solution1(nums);
         assertArrayEquals(new int[] {0,0,0,0}, nums);
     }
 
     @Test
-    void moveZeroes_example5() {
+    void moveZeroes_solution1_example5() {
         int[] nums = {};
         MoveZeros mz = new MoveZeros();
-        mz.moveZeroes(nums);
+        mz.moveZeroes_solution1(nums);
+        assertArrayEquals(new int[]{}, nums);
+    }
+
+    // Solution 2 - Tests
+    @Test
+    void moveZeroes_solution2_example1() {
+        int[] nums = {0, 1, 0, 3, 12};
+        MoveZeros mz = new MoveZeros();
+        mz.moveZeroes_solution2(nums);
+        assertArrayEquals(new int[] {1,3,12,0,0}, nums);
+    }
+
+    @Test
+    void moveZeroes_solution2_example2() {
+        int[] nums = {0};
+        MoveZeros mz = new MoveZeros();
+        mz.moveZeroes_solution2(nums);
+        assertArrayEquals(new int[] {0}, nums);
+    }
+
+    @Test
+    void moveZeroes_solution2_example3() {
+        int[] nums = {1,2,3,4};
+        MoveZeros mz = new MoveZeros();
+        mz.moveZeroes_solution2(nums);
+        assertArrayEquals(new int[] {1,2,3,4}, nums);
+    }
+
+    @Test
+    void moveZeroes_solution2_example4() {
+        int[] nums = {0,0,0,0};
+        MoveZeros mz = new MoveZeros();
+        mz.moveZeroes_solution2(nums);
+        assertArrayEquals(new int[] {0,0,0,0}, nums);
+    }
+
+    @Test
+    void moveZeroes_solution2_example5() {
+        int[] nums = {};
+        MoveZeros mz = new MoveZeros();
+        mz.moveZeroes_solution2(nums);
         assertArrayEquals(new int[]{}, nums);
     }
 }
