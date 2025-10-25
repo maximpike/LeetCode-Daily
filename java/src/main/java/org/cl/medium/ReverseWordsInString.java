5b1f60738208069f0c21645b4655656268835b51
maxpike <maximilianpike@gmail.com>
2025-10-25 22:39:30 +0200

    refactor: code clean up for reverse words in string

--- java/src/main/java/org/cl/medium/ReverseWordsInString.java
+++ java/src/main/java/org/cl/medium/ReverseWordsInString.java
@@ -2,13 +2,12 @@ package org.cl.medium;
 
 public class ReverseWordsInString {
 
-    // -------------------- SOLUTION 1 - dont use StingBuilder  ---------------------------//
+    // -------------------- SOLUTION 1 ---------------------------//
     public String reverseWords_solution1(String s) {
 
         String[] words = s.trim().split("\\s+");
         int l = 0;
         int r = words.length - 1;
-
         while (l < r) {
             String temp = words[l];
             words[l] = words[r];
@@ -18,7 +17,7 @@ public class ReverseWordsInString {
         }
         return String.join(" ", words);
     }
-    // Time Complexity: O(3n) = O(n)
+    // Time Complexity: O(4n) = O(n)
     // Space Complexity: O(2n) = O(n) [Aux space: O(n)]
 
     // -------------------- SOLUTION 2 - O(1) space ---------------------------//
@@ -29,9 +28,8 @@ public class ReverseWordsInString {
     // -------------- SOLUTION 3 - Built in functions ---------------------//
     public String reverseWords_solution3(String s) {
 
-        String[] words = s.trim().split("\\s+"); // Split optimization - \\s+ automatically ignores multiple spaces
+        String[] words = s.trim().split("\\s+"); // Split optimisation - \\s+ automatically ignores multiple spaces
         StringBuilder result = new StringBuilder();
-
         for (int i = words.length - 1; i >= 0; i--) {
             result.append(words[i]);
             if (i > 0) {
@@ -40,6 +38,6 @@ public class ReverseWordsInString {
         }
         return result.toString(); // O(n)
     }
-    // Time Complexity: O(3n) = O(n)
+    // Time Complexity: O(4n) = O(n)
     // Space Complexity: O(3n) = O(n) [Aux space: O(2n) = O(n)]
 }
